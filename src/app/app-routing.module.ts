781cc4b45763620469c41a999077024205f81d20
Johnny <xjohn117@outlook.com>
2021-05-06 14:53:12 -0500

    puse la primera ruta de cronograma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { CronogramaComponent } from './components/cronograma/cronograma.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'post/:cronograma',
+    component:CronogramaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
