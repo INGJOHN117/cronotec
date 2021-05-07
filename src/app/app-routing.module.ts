c6af33c212a2d896d00231158ecb5b2ce7f069c4
Johnny <xjohn117@outlook.com>
2021-05-07 00:10:46 -0500

    se agrega el menu de  navegacion funcional  con alguna de los componentes que a su ves seran vistas del proyecto se configuran las rutas y en adelante solo se debe agregar la rutas en el routing del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { CronogramaComponent } from './components/cronograma/cronograma.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistroEquipoComponent } from './components/registro-equipo/registro-equipo.component';
+import { RegistroSoporteComponent } from './components/registro-soporte/registro-soporte.component';
+import { HojasDeVidaComponent } from './components/hojas-de-vida/hojas-de-vida.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {
-    path:'post/:cronograma',
-    component:CronogramaComponent
-  }
+  {path:'cronograma', component:CronogramaComponent},
+  {path:'registroEquipo', component:RegistroEquipoComponent},
+  {path:'registroSoporte', component:RegistroSoporteComponent},
+  {path:'hojasDeVida', component:HojasDeVidaComponent},
+  {path:'login', component:LoginComponent}
 ];
 
 @NgModule({
